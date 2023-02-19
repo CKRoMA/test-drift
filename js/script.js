ec03af58a17577755ec13eb5744201fb716efb3c
CKRoMA <1@1.com>
2023-02-19 18:44:21 +0300

    animation

--- js/script.js
+++ js/script.js
@@ -71,3 +71,54 @@ $(document).ready(function () {
 
 
 });
+
+const animItems = document.querySelectorAll(`._anim-items`)
+if (animItems.length > 0) {
+	window.addEventListener(`scroll`, animOnScroll)
+
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index]
+			const animItemHeight = animItem.offsetHeight
+			const animItemOffSet = offset(animItem).top
+			const animStart = 4
+			let animItemPoint = window.innerHeight - animItemHeight / animStart
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart
+			}
+			if ((pageYOffset > animItemOffSet - animItemPoint) && pageYOffset < (animItemOffSet + animItemHeight)) {
+				animItem.classList.add(`_active`)
+			} else {
+				if (!(animItem.classList.contains(`_anim-no-hide`))) {
+					animItem.classList.remove(`_active`)
+				}
+			}
+		}
+	}
+
+	function offset(el) {
+		const rect = el.getBoundingClientRect()
+		let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+		let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	setTimeout(() => {
+		animOnScroll()
+	}, 300)
+}
+
+const links = document.querySelectorAll('.product-line__content-item');
+
+for (let link of links) {
+	link.addEventListener('click', () => {
+		let activeLink = document.querySelector('.product-line__content-item._active');
+		if (activeLink) {
+			activeLink.classList.remove('_active');
+		}
+		link.classList.add('_active');
+	})
+}
+
+
+
